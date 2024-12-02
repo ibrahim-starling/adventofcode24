0f4890f35a401097b83134f7a8824847f69af82d
Ibrahim Hassan <ibrahim.hassan@starlingbank.com>
2024-12-02 11:11:06 +0000

    day1 - completed

--- src/day1/day1.rb
+++ src/day1/day1.rb
@@ -1,23 +1,49 @@
 # frozen_string_literal: true
 
-array1 = []
-array2 = []
+def part1(file_name)
+  array1 = []
+  array2 = []
 
-File.foreach('input.txt') do |line|
-  num1, num2 = line
-                 .strip
-                 .split("\s")
-  array1.push(num1.to_i)
-  array2.push(num2.to_i)
+  File.foreach(file_name) do |line|
+    num1, num2 = line
+                   .strip
+                   .split("\s")
+    array1.push(num1.to_i)
+    array2.push(num2.to_i)
+  end
+
+  array1.sort!
+  array2.sort!
+
+  sum = 0
+  array1.each_index do |x|
+    sum += (array1[x] - array2[x]).abs
+  end
+
+  sum
 end
 
-# Sort Arrays in place
-array1.sort!
-array2.sort!
+def part2(file_name)
+  array1 = []
+  array2 = []
 
-sum = 0
-array1.each_index do |x|
-  sum += (array1[x] - array2[x]).abs
+  File.foreach(file_name) do |line|
+    num1, num2 = line
+                   .strip
+                   .split("\s")
+    array1.push(num1.to_i)
+    array2.push(num2.to_i)
+  end
+
+  sum = 0
+  array2_count = array2.tally
+
+  array1.each do |num|
+    sum += num * array2_count.fetch(num, 0)
+  end
+
+  sum
 end
 
-puts sum
+puts(part1 'input.txt')
+puts(part2 'input.txt')
